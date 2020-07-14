0dac15eee89824e86773fc5cd0faa376f3854c9a
nzyumek <nzyume.k@gmail.com>
2020-07-14 03:22:42 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+  def full_title(page_title = '')
+    base_title = "明治大学アナウンス研究会"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+    
 end
--- test/controllers/mak_controller_test.rb
+++ test/controllers/mak_controller_test.rb
@@ -2,14 +2,11 @@ require 'test_helper'
 
 class MakControllerTest < ActionDispatch::IntegrationTest
   
-  def setup
-    @base_title = "明治大学アナウンス研究会"
-  end
   
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Home | 明治大学アナウンス研究会"
   end
 
 
